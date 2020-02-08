9c15dc0df0a60b1694d2c7fc07ee51b925c69321
Yali Miranda <yalimiranda@gmail.com>
2020-02-08 01:34:43 -0800

    Updates

---
+++ precache-manifest.ad85ac790fda71d96a3af92eebdcfad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9dbbacca6a829f86be4001245d4e6b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f23b9187e90e97459a38",
+    "url": "/clicky-game/static/css/main.54e936f8.chunk.css"
+  },
+  {
+    "revision": "ffb5d5bda513ee869b6f",
+    "url": "/clicky-game/static/js/2.9ae1bd12.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.9ae1bd12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23b9187e90e97459a38",
+    "url": "/clicky-game/static/js/main.dc1f231d.chunk.js"
+  },
+  {
+    "revision": "e3904828df4a4c033863",
+    "url": "/clicky-game/static/js/runtime-main.b73269a7.js"
+  }
+]);
